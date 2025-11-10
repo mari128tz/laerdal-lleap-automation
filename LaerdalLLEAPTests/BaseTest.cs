2d165f0ee78a1381eca0d69bee405708cc84ccbc
Mariana <mari.oliv00@pm.me>
2025-11-10 17:23:01 +0100

    Initial project setup with base structure

---
+++ LaerdalLLEAPTests/BaseTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace LaerdalLLEAPTests
+{
+    public class BaseTest
+    {
+        [Test]
+        public void SetupVerificationTest()
+        {
+            Assert.Pass("Rider project setup is working correctly!");
+        }
+    }
+}
